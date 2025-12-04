37f18d2355ec1fd7bc96c0e4628f99a016111bce
Viacheslav Nefedov <vnefedov@nefedov.net>
2025-12-04 11:40:32 +0200

    lab4 with AI-agents added

---
+++ labs/lab4/README.md
@@ -0,0 +1,7 @@
+Generate uv.lock with `uv sync`
+
+Build the image with `docker buildx build --load -t cybersec-agent-devui .` where:
+
+- `--load` - load the image into local Docker registry
+- `-t cybersec-agent-devui` - image tag`
+- `.` - path to Dockerfile
---
+++ labs/lab4/app/hello_world/agent.py
@@ -0,0 +1,24 @@
+import os
+
+from agent_framework import ChatAgent
+from agent_framework.openai import OpenAIChatClient
+
+# КОНФИГ
+base_url = os.getenv("API_BASE_URL", "https://openrouter.ai/api/v1")
+api_key = os.getenv("API_KEY")
+model_id = os.getenv("MODEL", "x-ai/grok-4.1-fast:free")
+
+client = OpenAIChatClient(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+    model_id="x-ai/grok-4.1-fast:free",
+)
+
+agent = ChatAgent(
+    chat_client=client,
+    name="hello-world-agent",
+    instructions="""
+        You're a friendly agent.
+        Ask the user for their name and greet them personally.
+    """
+)
